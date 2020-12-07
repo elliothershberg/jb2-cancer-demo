11a3ca2364d95bde64e89f1fec9c464eb5e325ce
Elliot Hershberg <eahershberg@gmail.com>
2020-12-07 12:55:26 -0800

    Add app headerbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import icon from './icon.ico'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    width: 48,
+    height: 48,
+    marginRight: 12,
+  },
+  title: {
+    textAlign: 'center',
+    flexGrow: 1,
+  },
+  toolbar: {
+    background: '#0d233f',
+  },
+}))
+
+function Header() {
+  const classes = useStyles()
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography className={classes.title} variant="h3">
+          <img alt="logo" src={icon} className={classes.icon} />
+          JBrowse 2 Cancer Demo
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
